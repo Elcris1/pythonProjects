3ed1cb28c8e09c233f1be10afcf899550313d985
Elcris1 <60231775+Elcris1@users.noreply.github.com>
2023-03-01 14:03:35 +0100

    Add files via upload

---
+++ talon Skin Renamer.py
@@ -0,0 +1,48 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    directory = input("Introduce the directory: ")
+    entries = os.listdir(directory) # Getting the list of files
+    print("Replacing directory:", directory)
+    # Code for every file in directory
+    for entry in entries:
+        # if not a file -> Directory
+        if "." not in entry:
+            # Getting new directory name
+            newDirectory = directory + "\\" + entry
+            print("Directory found:", newDirectory)
+            # Getting the list of files in new directory
+            newFiles = os.listdir(newDirectory)
+            for file in newFiles:
+                # Replacing name for the file
+                newFileName = file.replace(".pie_c_12_9", "")
+                print("\t Renaming", file, "for", newFileName)
+
+                # Creating old and new directories for the os module
+                oldFileDirectory = newDirectory + "\\" + file
+                newFileDirectory = newDirectory + "\\" + newFileName
+
+                # Renaming the file
+                os.rename(oldFileDirectory, newFileDirectory)
+
+            print(newDirectory, "already replaced")
+        else:
+            # Replacing the name for the file
+            newEntryName = entry.replace(".pie_c_12_9", "")
+            print("\t Renaming", entry, "for", newEntryName)
+
+            # Creating the new and the old directories for the os module
+            oldFileDirectory = directory + "\\" + entry
+            newFileDirectory = directory + "\\" + newEntryName
+
+            # Renaming the file
+            os.rename(oldFileDirectory, newFileDirectory)
+
+    print(directory, "Replaced")
+
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
